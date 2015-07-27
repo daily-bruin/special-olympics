f1a9f7df0364db9ad5a2041f3d322bd119db6c4d
shannonphu <shannonphu@sbcglobal.net>
2015-07-27 15:07:40 -0700

    put scroll speed back into index.html

--- js/main.js
+++ js/main.js
@@ -94,7 +94,7 @@ $(document).ready(function() {
 	    var $target = $(target);
 
 	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset()/*.top*/ - originalContentHeight - 90
+	        'scrollTop': $target.offset().top - originalContentHeight - 90
 	    }, 1000, 'swing', function () {
 	        window.location.hash = target;
 	    });
